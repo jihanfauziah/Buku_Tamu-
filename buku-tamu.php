8a4b88ff6e57823af99d9da7ef9335b895c2a0dd
Rahma Santika Al Anshori <Rahmasantikaansori01.com>
2025-10-18 20:44:21 +0700

    menambahkan halaman buku tamu dan fitur menampilkan data tamu

---
+++ buku-tamu.php
@@ -0,0 +1,72 @@
+<?php
+require_once('function.php');
+include_once('templates/header.php');
+?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+<!-- Page Heading -->
+<h1 class="h3 mb-4 text-gray-800">Buku Tamu</h1>
+
+</div>
+<!-- /.container-fluid -->
+
+
+
+ <!-- DataTales Example -->
+ <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Daftar Tamu</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Tanggal</th>
+                                            <th>Nama Tamu</th>
+                                            <th>Alamat</th>
+                                            <th>Bertemu Dengan</th>
+                                            <th>Kepentingan</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $no = 1;
+                                        $buku_tamuu = query("SELECT * FROM buku_tamuu");
+                                        foreach($buku_tamuu as $tamu) : ?>
+                                        <tr>
+                                            <td><?= $no++; ?></td>
+                                            <td><?= $tamu['tanggal']; ?></td>
+                                            <td><?= $tamu['nama_tamu']; ?></td>
+                                            <td><?= $tamu['alamat']; ?></td>
+                                            <td><?= $tamu['no_hp']; ?></td>
+                                            <td><?= $tamu['bertemu']; ?></td>
+                                            <td><?= $tamu['kepentingan']; ?></td>
+                                            <td><button class="btn btn-success" type="button">Ubah</button>
+                                                <button class="btn btn-danger" type="button">Hapus</button></td>
+                                        </tr>
+                                        <?php endforeach; ?>
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Tanggal</th>
+                                            <th>Nama Tamu</th>
+                                            <th>Alamat</th>
+                                            <th>Bertemu Dengan</th>
+                                            <th>Kepentingan</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+<?php
+include_once('templates/footer.php');
+?>
